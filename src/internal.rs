3f7a822f4888fc233306b8fb5c54cc2b8af70ddb
Martin Wernstål <m4rw3r@gmail.com>
2015-11-16 23:30:10 +0100

    Rustdoc

--- src/internal.rs
+++ src/internal.rs
@@ -1,10 +1,16 @@
 //! Module used to construct fundamental parsers and combinators.
+//! 
+//! # Internal
+//!
+//! Only used by fundamental parsers and combinators.
 
 use ::{ParseResult, Input};
 
-/// Private inner type containing the parse-state.
-/// 
-/// Only used to create fundamental parsers and combinators.
+/// Internal inner type containing the parse-state.
+///
+/// # Internal
+///
+/// Only used by fundamental parsers and combinators.
 #[must_use]
 #[derive(Debug, Eq, PartialEq)]
 pub enum State<'a, I: 'a, T, E>
@@ -16,9 +22,11 @@ pub enum State<'a, I: 'a, T, E>
     Incomplete(usize),
 }
 
-/// Trait for modifying `ParseResult`.
+/// Internal trait for modifying `ParseResult`.
 /// 
-/// Only use this to create fundamental combinators.
+/// # Internal
+///
+/// Only used by fundamental parsers and combinators.
 pub trait ParseResultModify<'a> {
     type Input;
     type Data;
@@ -32,8 +40,7 @@ pub trait ParseResultModify<'a> {
             <Self as ParseResultModify<'a>>::Data:  'a,
             <Self as ParseResultModify<'a>>::Error: 'a;
 
-    /// Modifies the content of ParseResult, allowing types to change.
-    // TODO: Is this needed?
+    /// Applies the function `f` to the inner `State`, allows modification of data and error types.
     #[inline(always)]
     fn parse<I, T, E, F>(self, F) -> ParseResult<'a, I, T, E>
       where F: FnOnce(State<'a, Self::Input, Self::Data, Self::Error>) -> ParseResult<'a, I, T, E>,
@@ -46,11 +53,17 @@ pub trait ParseResultModify<'a> {
     fn internal(self) -> State<'a, Self::Input, Self::Data, Self::Error>;
 }
 
+/// Implementation of internal trait used as a building block for combinators.
+/// 
+/// # Internal
+///
+/// Only used by fundamental parsers and combinators.
 impl<'a, I, T, E> ParseResultModify<'a> for ParseResult<'a, I, T, E> {
     type Input = I;
     type Data  = T;
     type Error = E;
 
+    /// Applies the function `f` to the inner `State` while preserving types.
     #[inline(always)]
     fn modify<F>(self, f: F) -> Self
       where F: FnOnce(State<'a, Self::Input, Self::Data, Self::Error>) -> ParseResult<'a, Self::Input, Self::Data, Self::Error>,
@@ -60,6 +73,7 @@ impl<'a, I, T, E> ParseResultModify<'a> for ParseResult<'a, I, T, E> {
         f(self.0)
     }
 
+    /// Applies the function `f` to the inner `State`, allows modification of data and error types.
     #[inline(always)]
     fn parse<N, Y, R, F>(self, f: F) -> ParseResult<'a, N, Y, R>
       where F: FnOnce(State<'a, Self::Input, Self::Data, Self::Error>) -> ParseResult<'a, N, Y, R>,
@@ -69,6 +83,7 @@ impl<'a, I, T, E> ParseResultModify<'a> for ParseResult<'a, I, T, E> {
         f(self.0)
     }
 
+    /// Consumes the `ParseResult` and reveals the inner state.
     #[inline(always)]
     fn internal(self) -> State<'a, Self::Input, Self::Data, Self::Error> {
         self.0
@@ -77,7 +92,9 @@ impl<'a, I, T, E> ParseResultModify<'a> for ParseResult<'a, I, T, E> {
 
 /// Trait for modifying `Input`.
 /// 
-/// Only use this to create fundamental parsers and combinators.
+/// # Internal
+///
+/// Only used by fundamental parsers and combinators.
 pub trait InputModify<'a> {
     type Type;
 
@@ -94,34 +111,22 @@ pub trait InputModify<'a> {
             <Self as InputModify<'a>>::Type: 'a;
 }
 
-/// Creates a data-carrying value from an input slice and a data value.
-#[inline(always)]
-pub fn data<'a, I, T, E>(i: &'a [I], t: T) -> ParseResult<'a, I, T, E> {
-    ParseResult(State::Data(i, t))
-}
-
-/// Creates an error value from an input slice and an error value.
-#[inline(always)]
-pub fn error<'a, I, T, E>(i: &'a [I], e: E) -> ParseResult<'a, I, T, E> {
-    ParseResult(State::Error(i, e))
-}
-
-/// Notifies the combinator that a parser has reached the end of the currently supplied slice but
-/// requires more data.
-#[inline(always)]
-pub fn incomplete<'a, I, T, E>(n: usize) -> ParseResult<'a, I, T, E> {
-    ParseResult(State::Incomplete(n))
-}
-
+/// Implementation of internal trait used to build parsers and combinators.
+/// 
+/// # Internal
+///
+/// Only used by fundamental parsers and combinators.
 impl<'a, I> InputModify<'a> for Input<'a, I> {
     type Type = I;
 
+    /// Modifies the inner data without leaving the `Input` context.
     #[inline(always)]
     fn modify<F>(self, f: F) -> Self
       where F: FnOnce(&'a [Self::Type]) -> &'a [Self::Type] {
         Input(f(self.0))
     }
 
+    /// Applies a parser operating directly on &[Type] to the input.
     #[inline(always)]
     fn parse<F, T, E>(self, f: F) -> ParseResult<'a, Self::Type, T, E>
       where F: FnOnce(&'a [Self::Type]) -> ParseResult<'a, Self::Type, T, E> {
@@ -129,3 +134,33 @@ impl<'a, I> InputModify<'a> for Input<'a, I> {
     }
 }
 
+/// Creates a data-carrying value from an input slice and a data value.
+/// 
+/// # Internal
+///
+/// Only used by fundamental parsers and combinators.
+#[inline(always)]
+pub fn data<'a, I, T, E>(i: &'a [I], t: T) -> ParseResult<'a, I, T, E> {
+    ParseResult(State::Data(i, t))
+}
+
+/// Creates an error value from an input slice and an error value.
+/// 
+/// # Internal
+///
+/// Only used by fundamental parsers and combinators.
+#[inline(always)]
+pub fn error<'a, I, T, E>(i: &'a [I], e: E) -> ParseResult<'a, I, T, E> {
+    ParseResult(State::Error(i, e))
+}
+
+/// Notifies the combinator that a parser has reached the end of the currently supplied slice but
+/// requires more data.
+/// 
+/// # Internal
+///
+/// Only used by fundamental parsers and combinators.
+#[inline(always)]
+pub fn incomplete<'a, I, T, E>(n: usize) -> ParseResult<'a, I, T, E> {
+    ParseResult(State::Incomplete(n))
+}
