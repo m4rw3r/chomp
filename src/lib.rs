7e933a6ddfc2fa99f1d171cdfe6ce667018093c9
Kristoffer Semelka <soderstroff@gmail.com>
2015-11-15 20:21:10 -0800

    Added introductory documentation and a minor tutorial.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
-//! Library providing a parser combinator.
+//! Chomp is an alternative parser combinator library for the Rust programming language. It was written as the culmination of the experiments detailed in these blog posts:
+//! * [Part 1](http://m4rw3r.github.io/parser-combinator-experiments-rust/)
+//! * [Part 2](http://m4rw3r.github.io/parser-combinator-experiments-errors)
+//! * [Part 3](http://m4rw3r.github.io/parser-combinator-experiments-part-3)
+//!
+//! For its current capabilities, you will find that Chomp performs consistently as well, if not better, than optimized C parsers, while being vastly more expressive. For an example that builds a performant HTTP parser out of smaller parsers, see [http_parser.rs](examples/http_parser.rs).
+//! # Example
 //!
 //! ```
 //! use chomp::{Input, ParseResult, Error};
@@ -24,6 +30,91 @@
 //!
 //! assert_eq!(name(i).unwrap(), Name{first: b"martin", last: "wernstål".as_bytes()});
 //! ```
+//! # Usage
+//! Chomp's functionality is split between three modules.
+//!* `parsers` contains the basic parsers used to parse streams of input.
+//!* `combinators` contains functions which take parsers and return new ones.
+//!* `internal` contains the building blocks used to make new parsers. This is advanced usage and is far more involved than using the preexisting parsers, but is sometimes unavoidable.
+//!
+//!    A parser is, at its simplest, a function that takes a slice of input and returns a `ParserResult<'a, I, T, E>`, where I, T, and E are the input, output, and error types, respectively. Parsers are usually parameterized over values or other parsers as well, so these appear as extra arguments in the parsing function. As an example, here is the signature of the `token` parser, which matches a particular input.
+//!
+//!```
+//!fn token<'a, I: 'a + Copy + Eq>(i: Input<'a, I>, t: I) -> SimpleResult<'a, I, I> {...}
+//!```
+//!
+//!    Notice that the first argument is an `Input<'a, I>`, and the second argument is some `I`. `Input<'a,I>` is just a datatype over the current state of the parser and a slice of input `I`, and prevents the parser writer from accidentally mutating the state of the parser. Later, when we introduce the `parse!` macro, we will see that using a parser in this macro just means supplying all of the arguments but the input, as so:
+//!```
+//!token(b'T');
+//!```
+//!Note that you cannot do this outside of the `parse!` macro.
+//!`SimpleResult<'a, I, T>` is a convenience type alias over Result<'a, I, T, Error<u8>>, and `Error<I>` is just a convenient "default" error type that will satisfy most users. For more sophisticated usage, one can always write a custom error type.
+//!
+//!    A very useful parser is the `satisfy` parser:
+//!```
+//!fn satisfy<'a, I: 'a + Copy, F>(i: Input<'a, I>, f: F) -> SimpleResult<'a, I, I>
+//!    where F: FnOnce(I) -> bool { ... }
+//!```
+//!
+//!Besides the input state, satisfy's only parameter is a predicate function and will succeed only if the next piece of input satisfies the supplied predicate. Here's an example that might be used in the `parse` macro.
+//!
+//!```
+//!    satisfy(|c| {
+//!        match c {
+//!            b'c' | b'h' | b'a' | b'r' => true,
+//!            _ => false,
+//!        }
+//!    })
+//!```
+//!    This parser will only succeed if the character is one of the characters in "char".
+//!
+//!    Lastly, here is the parser combinator `count`, which will attempt to run a parser a number of times on its input.
+//!
+//!```
+//!pub fn count<'a, I, T, E, F, U>(i: Input<'a, I>, num: usize, p: F) -> ParseResult<'a, I, T, E>
+//!  where I: Copy,
+//!        U: 'a,
+//!        F: FnMut(Input<'a, I>) -> ParseResult<'a, I, U, E>,
+//!        T: FromIterator<U> { ... }
+//!```
+//!This combinator construcs an iterator that will match elements `num` times, and collect them into any datatype you want.
+
+//!Using parsers is almost entirely done using the `parse!` macro, which enables us to do three distinct things:
+//!* Sequence parsers over the remaining input
+//!* Store intermediate results into datatypes
+//!* Return a datatype at the end, which may be the result of any arbitrary computation over the intermediate results.
+
+//!In other words, just as a normal Rust function usually looks something like
+//!```
+//!fn f() -> (u8, u8, u8) {
+//!    let a = 3;
+//!    let b = 3;
+//!    launch_missiles();
+//!    return (a, b, a + b);
+//!}
+//!```
+//!
+//!A Chomp parser looks something like
+//!
+//!```
+//!fn f<'a>(i: Input<'a, u8>) -> SimpleResult<'a, I, (u8, u8, u8)> {
+//!    parse!{i;
+//!        let a = token(b'3');
+//!        let b = token(b'3')
+//!        string(b"missiles");
+//!        ret (a, b, a + b)
+//!    }
+//!}
+//!```
+//!Readers familiar with Haskell or F# will recognize this as a "monadic computation" or "computation expression".
+//!
+//!    You use the `parse!` macro as follows:
+//!- Write the input parameter first, with a semicolon.
+//!- Write any number of valid parser actions or identifier bindings, where:
+//!    - a parser action takes the form `parser(params*)`, with the input parameter omitted.
+//!    - an identifier binding takes the form `let identifer = parser(params*);`, with the input parameter omitted.
+//!- Write the final line of the macro, which must always be either a parser action, or a return statement which takes the form `ret expression`. The type of `expression` becomes the return type of the entire parser, should it succeed.
+//!The entire grammar for the macro is listed elsewhere in this documentation.
+
 use ::std::fmt;
 
 #[macro_use]
