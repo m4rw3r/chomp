f06de1d1c56e02ff92f293edaa94623f7baf31f9
Martin Wernstål <m4rw3r@gmail.com>
2015-11-19 23:06:13 +0100

    Compile-fail tests: Added -L target/debug/deps to rustcflags

--- tests/compile-fail_test.rs
+++ tests/compile-fail_test.rs
@@ -10,7 +10,7 @@ fn run_mode(mode: &'static str) {
 
     config.mode = cfg_mode;
     config.src_base = PathBuf::from(format!("tests/{}", mode));
-    config.target_rustcflags = Some("-L target/debug".to_string());
+    config.target_rustcflags = Some("-L target/debug -L target/debug/deps".to_string());
 
     compiletest::run_tests(&config);
 }
