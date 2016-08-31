acd4662213e29efd303571b23d61274a22707bcf
Martin Wernstål <m4rw3r@gmail.com>
2016-08-31 15:42:04 +0200

    Updated ascii compile-fail to use the monad syntax

--- tests/compile-fail/ascii_signed_unsigned_type4.rs
+++ tests/compile-fail/ascii_signed_unsigned_type4.rs
@@ -1,13 +1,14 @@
 // error-pattern:error[E0271]: type mismatch resolving `<i8 as conv::ValueFrom<u8>>::Err == conv::errors::NoError`
 
+#![feature(conservative_impl_trait)]
 extern crate chomp;
 
-use chomp::prelude::{U8Input, SimpleResult, parse_only};
+use chomp::prelude::{U8Input, Parser, Error, parse_only};
 use chomp::ascii::{signed, decimal};
 
-// Should not be possible to use unsigned integers with signed
-fn parser<I: U8Input>(i: I) -> SimpleResult<I, i8> {
-    signed(i, decimal)
+// Should not be possible to use unsigned integers with signed equal or smaller
+fn parser<I: U8Input>() -> impl Parser<I, Output=i8, Error=Error<u8>> {
+    signed(decimal())
 }
 
 fn main() {
