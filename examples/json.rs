0c62b16efc69dce4ff0da355d64d7e38468c134d
Martin Wernstål <m4rw3r@gmail.com>
2016-09-16 16:35:33 +0200

    examples/json: Use the new float-parser

--- examples/json.rs
+++ examples/json.rs
@@ -1,11 +1,10 @@
 extern crate chomp;
 
 use std::collections::HashMap;
-use std::mem::transmute;
 
-use chomp::ascii::{is_digit, is_whitespace};
-use chomp::combinators::{matched_by, or, option, sep_by};
-use chomp::parsers::{SimpleResult, any, token, scan, skip_while, skip_while1, string};
+use chomp::ascii::{float, is_whitespace};
+use chomp::combinators::{or, sep_by};
+use chomp::parsers::{any, token, scan, skip_while, string};
 use chomp::parsers::Error as ChompError;
 use chomp::types::{Buffer, Input, ParseResult};
 
@@ -30,43 +29,11 @@ pub fn parse<I: Input<Token=u8>>(i: I) -> ParseResult<I, Value, Error> {
         parse_object(i).map(Value::Object),        |i| or(i, |i|
         parse_array(i).map(Value::Array),          |i| or(i, |i|
         parse_string(i).map(Value::String),        |i| or(i, |i|
-        parse_number(i).map(Value::Number),        |i| or(i, |i|
+        float(i).map(Value::Number),               |i| or(i, |i|
         string(i, b"true").map(|_|  Value::True),  |i| or(i, |i|
         string(i, b"false").map(|_| Value::False), |i| or(i, |i|
         string(i, b"null").map(|_|  Value::Null),  |i|
         i.err(Error::unexpected())))))))))
-}
-
-// AKA match_float + FromStr
-//
-// TODO: Move to ascii module with proper tests
-/// Parses a floating point number from the input
-fn parse_number<I: Input<Token=u8>>(i: I) -> ParseResult<I, f64, Error> {
-    fn sign<I: Input<Token=u8>>(i: I) -> SimpleResult<I, u8> {
-        or(i, |i| token(i, b'+'), |i| token(i, b'-'))
-    }
-    fn signed_decimal<I: Input<Token=u8>>(i: I) -> SimpleResult<I, ()> {
-        option(i, sign, b'+').then(|i| skip_while1(i, is_digit))
-    }
-    fn e<I: Input<Token=u8>>(i: I) -> SimpleResult<I, u8> {
-        or(i, |i| token(i, b'e'), |i| token(i, b'E'))
-    }
-    fn make_num<B: Buffer<Token=u8>>(b: B) -> f64 {
-        // If we add a restriction that Input<Buffer=&[u8]> then we can skip this allocation,
-        // or if we reimplement float-parsing properly (or find a library where we can supply
-        // numbers instead of a string-slice.
-        let v = b.to_vec();
-
-        // v only contains [-+0-9.eE], utf-8 safe
-        let s: &str = unsafe { transmute(&v[..]) };
-
-        s.parse().expect("Invalid floating number")
-    }
-
-    matched_by(i, |i|
-        signed_decimal(i).then(|i|
-        option(i, |i| token(i, b'.').then(|i| skip_while1(i, is_digit)), ()).then(|i|
-        option(i, |i| e(i).then(signed_decimal), ())))).map(|(b, _)| make_num(b))
 }
 
 /// Parse a quoted string
@@ -98,7 +65,7 @@ fn parse_object<I: Input<Token=u8>>(i: I) -> ParseResult<I, HashMap<String, Valu
 fn separator<I: Input<Token=u8>>(i: I) -> ParseResult<I, (), Error> {
     skip_while(i, is_whitespace).then(|i|
     token(i, b',').then(|i|
-    skip_while(i, is_whitespace))).map_err(From::from)
+    skip_while(i, is_whitespace)))
 }
 
 /// Parses string-key: value
