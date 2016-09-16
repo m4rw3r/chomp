4aef15cf0f937749064a9693d464e37fba918ca7
Martin Wernstål <m4rw3r@gmail.com>
2016-09-16 16:43:42 +0200

    benches: Added more float-versions

--- benches/float.rs
+++ benches/float.rs
@@ -41,12 +41,37 @@ fn float_f32(b: &mut Bencher) {
     })
 }
 
+#[bench]
+fn float_small_f64(b: &mut Bencher) {
+    assert_eq!(ascii::float::<_, f64>(&b"1"[..]).into_inner(), (&b""[..], Ok(1.0)));
+
+    b.iter(|| {
+        ascii::float::<_, f64>(&b"1"[..])
+    })
+}
+
+#[bench]
+fn float_small_f32(b: &mut Bencher) {
+    assert_eq!(ascii::float::<_, f32>(&b"1"[..]).into_inner(), (&b""[..], Ok(1.0)));
+
+    b.iter(|| {
+        ascii::float::<_, f32>(&b"1"[..])
+    })
+}
+
 /// The purpose of this test is to measure the time Chomp uses to parse and allocate the vector
 /// required to pass the data on to Rust's `FromStr` implementation for `f32` and `f64`.
 #[bench]
 fn float_no_conversion(b: &mut Bencher) {
     b.iter(|| {
         ascii::match_float(&PI_SLICE[..]).map(|b| b.into_vec())
+    })
+}
+
+#[bench]
+fn float_small_no_conversion(b: &mut Bencher) {
+    b.iter(|| {
+        ascii::match_float(&b"1"[..]).map(|b| b.into_vec())
     })
 }
 
@@ -69,6 +94,31 @@ fn from_str_f64(b: &mut Bencher) {
 
     b.iter(|| {
         let f: Result<f64, _> = FromStr::from_str(unsafe { transmute(&PI_SLICE[..]) });
+
+        f
+    })
+}
+
+
+/// Reference, 32-bit, small
+#[bench]
+fn from_str_small_f32(b: &mut Bencher) {
+    assert_eq!(FromStr::from_str(unsafe { transmute(&b"1"[..]) }), Ok(1.0f32));
+
+    b.iter(|| {
+        let f: Result<f32, _> = FromStr::from_str(unsafe { transmute(&b"1"[..]) });
+
+        f
+    })
+}
+
+/// Reference, 64-bit
+#[bench]
+fn from_str_small_f64(b: &mut Bencher) {
+    assert_eq!(FromStr::from_str(unsafe { transmute(&b"1"[..]) }), Ok(1.0f64));
+
+    b.iter(|| {
+        let f: Result<f64, _> = FromStr::from_str(unsafe { transmute(&b"1"[..]) });
 
         f
     })
